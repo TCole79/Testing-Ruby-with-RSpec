50463ef8ef70171cf26b8531c6ed7bdddc09cad5
Tristan <tristanleighcole@googlemail.com>
2022-08-14 11:28:49 -0600

    Update card_spec.rb
    * add it and expect

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,7 @@
-RSpec.describe 'Card' do
+RSpec.describe 'Card' do # card class
   it 'has a type' do
     # this is exactly the same as using 'it' - specify 'has a type' do
+    card = Card.new('Ace of Spades')
+    expect(card.type).to eq('Ace of Spades')
   end
 end
